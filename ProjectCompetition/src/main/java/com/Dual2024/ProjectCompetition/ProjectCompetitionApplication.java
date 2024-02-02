891dbb41bba77b14397d20d153f86e82871f9f6f
Francisco-B-O <balonero18@gmail.com>
2024-02-02 14:35:39 +0100

    feat: :tada: Proyecto creado

---
+++ ProjectCompetition/src/main/java/com/Dual2024/ProjectCompetition/ProjectCompetitionApplication.java
@@ -0,0 +1,14 @@
+package com.Dual2024.ProjectCompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProjectCompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCompetitionApplication.class, args);
+	}
+
+}
---
+++ ProjectCompetition/src/test/java/com/Dual2024/ProjectCompetition/ProjectCompetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dual2024.ProjectCompetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCompetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
