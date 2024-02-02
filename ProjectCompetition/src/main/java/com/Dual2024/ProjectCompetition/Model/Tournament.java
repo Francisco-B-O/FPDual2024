61d94eb94dd96d164b6787fa3c41c9ba87baed9b
Francisco-B-O <balonero18@gmail.com>
2024-02-02 14:41:32 +0100

    feat: :sparkles: Creacion de la entidad Tournament

---
+++ ProjectCompetition/src/main/java/com/Dual2024/ProjectCompetition/Model/Tournament.java
@@ -0,0 +1,73 @@
+package com.Dual2024.ProjectCompetition.Model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "tournaments")
+public class Tournament {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "tournament_id")
+	private Long id;
+
+	@NotBlank
+	@Column(name = "tournament_name", nullable = false, unique = true)
+	private String name;
+
+	@NotNull
+	@Column(name = "tournament_size", nullable = false)
+	private Integer size;
+
+	@ManyToOne
+	@JoinColumn(name = "tournament_format_id", nullable = false)
+	private Format format;
+
+	@NotNull
+	@Column(name = "tournament_start_date", nullable = false)
+	private LocalDateTime startDate;
+
+	@NotNull
+	@Column(name = "tournament_end_date", nullable = false)
+	private LocalDateTime endDate;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tournament_state", nullable = false)
+	private TournamentState state;
+
+	@Lob
+	@Column(name = "tournament_logo")
+	private String logo;
+
+	@NotBlank
+	@Column(name = "tournament_description", nullable = false)
+	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "tournament_modality_id", nullable = false)
+	private Modality modality;
+
+	@ManyToMany(mappedBy = "tournaments")
+	private List<Team> teams;
+}
