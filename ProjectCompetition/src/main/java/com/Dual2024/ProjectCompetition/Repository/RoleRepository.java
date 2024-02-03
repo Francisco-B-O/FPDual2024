25fc93618071c23504e4b6f6c5d476243ab40670
Francisco-B-O <balonero18@gmail.com>
2024-02-03 13:07:33 +0100

    feat: :sparkles: Creacion del RoleRepository

---
+++ ProjectCompetition/src/main/java/com/Dual2024/ProjectCompetition/Repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.Dual2024.ProjectCompetition.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Dual2024.ProjectCompetition.Model.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Role findByName(String name);
+}
