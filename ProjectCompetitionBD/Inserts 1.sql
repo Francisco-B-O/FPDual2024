f6e2e7c2216bba7f8551733000780b2732e26917
Francisco-B-O <balonero18@gmail.com>
2024-02-29 14:27:18 +0100

    feat: :white_check_mark: Datos de prueba

--- ProjectCompetitionBD/Inserts 1.sql
+++ ProjectCompetitionBD/Inserts 1.sql
@@ -28,12 +28,12 @@ INSERT INTO modalities (modality_number_players, modality_name) VALUES
 (2,'Futbolín'),
 (3,'3x3');
 
-INSERT INTO teams (team_name, team_logo, team_modality_id) VALUES
-('Team01', '', 1),
-('Team02', '', 2),
-('Team03', '', 2),
-('Team04', '', 1),
-('Team05', '', 2);
+INSERT INTO teams (team_name, team_logo, team_modality_id,team_captain_id) VALUES
+('Team01', '', 1,1),
+('Team02', '', 2,2),
+('Team03', '', 2,3),
+('Team04', '', 1,4),
+('Team05', '', 2,5);
 
 INSERT INTO users_teams (users_teams_user_id, users_teams_team_id) VALUES
 (1,2),
@@ -53,11 +53,11 @@ INSERT INTO formats (format_name) VALUES
 INSERT INTO tournaments (tournament_name, tournament_size, tournament_start_date, 
 						 tournament_end_date, tournament_state, tournament_logo, 
 						 tournament_description, tournament_modality_id, tournament_format_id) VALUES
-('Tournament01', 20, CURRENT_DATE()+3, '2023-02-25', 'No comenzado', '', 'Liga de Futbolín de empresa', 1, 1),
-('Tournament02', 40, '2023-03-23', '2023-03-30', 'En juego', '', 'Torneo de Futbolín de empresa', 1, 2),
-('Tournament03', 32, '2022-07-31', '2022-08-09', 'Finalizado', '', 'Torneo de 3x3', 2, 3),
-('Tournament04', 10, CURRENT_DATE(), '2023-02-25', 'Suspendido', '', 'Liga 3x3', 2, 1),
-('Tournament05', 20, CURRENT_DATE(), '2023-06-20', 'Aplazado', '', 'Torneo de Futbolín de empresa', 1, 2);
+('Tournament01', 20, CURRENT_DATE()+3, '2023-02-25', 'EN_JUEGO', '', 'Liga de Futbolín de empresa', 1, 1),
+('Tournament02', 40, '2023-03-23', '2023-03-30', 'EN_JUEGO', '', 'Torneo de Futbolín de empresa', 1, 2),
+('Tournament03', 32, '2022-07-31', '2022-08-09', 'EN_JUEGO', '', 'Torneo de 3x3', 2, 3),
+('Tournament04', 10, CURRENT_DATE(), '2023-02-25', 'EN_JUEGO', '', 'Liga 3x3', 2, 1),
+('Tournament05', 20, CURRENT_DATE(), '2023-06-20', 'EN_JUEGO', '', 'Torneo de Futbolín de empresa', 1, 2);
 
 INSERT INTO tournaments_teams (tournaments_teams_tournament_id, tournaments_teams_team_id) VALUES
 (1,1),
