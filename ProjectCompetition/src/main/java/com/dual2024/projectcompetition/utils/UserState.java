ce274f1f2ddc228a26df1e6bbf670d140349ec01
Francisco-B-O <balonero18@gmail.com>
2024-03-14 13:46:15 +0100

    fix: :bug: Correccion UserState

---
+++ ProjectCompetition/src/main/java/com/dual2024/projectcompetition/utils/UserState.java
@@ -0,0 +1,21 @@
+package com.dual2024.projectcompetition.utils;
+
+/**
+ * The enum User state.
+ *
+ * @author Franciosco Balonero Olivera
+ */
+public enum UserState {
+    /**
+     * Conectado user state.
+     */
+    CONNECTED,
+    /**
+     * Desactivado user state.
+     */
+    DISABLED,
+    /**
+     * Desconectado user state.
+     */
+    DISCONNECTED
+}
